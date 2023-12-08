04c52f49b165bedae3c5b55d25bf1f47f8db34bd
Atul Garg <atulgarg4296@gmail.com>
2023-12-08 11:02:29 +0530

    Update main.js

--- Inception/main.js
+++ Inception/main.js
@@ -1,4 +1,5 @@
 const heading = React.createElement("h2",{id: "heading"},"Hello React");
         const root = ReactDOM.createRoot(document.getElementById("root1"));
         root.render(heading);
+Hello world
         
