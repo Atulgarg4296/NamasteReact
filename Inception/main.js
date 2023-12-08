2fcc7c938a7c355f99369af2e916e23ff74252b5
unknown <atulgarg4296@gmail.com>
2023-12-08 10:14:48 +0530

    Created NamsteReact Repo

---
+++ Inception/main.js
@@ -0,0 +1,4 @@
+const heading = React.createElement("h2",{id: "heading"},"Hello React");
+        const root = ReactDOM.createRoot(document.getElementById("root1"));
+        root.render(heading);
+        
